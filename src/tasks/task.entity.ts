6c861ce76749345f5d9a83f490c25b294911c8d9
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-05-25 06:50:25 -0300

    Tasks and Users - Database Relation

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { TaskStatus } from './dto/update-task.dto';
+import { User } from "src/auth/user.entity";
 
 @Entity()
 export class Task extends BaseEntity {
@@ -14,4 +15,7 @@ export class Task extends BaseEntity {
 
     @Column()
     status: TaskStatus;
+
+    @ManyToOne(type => User, user => user.tasks, { eager: false })
+    user: User;
 }
