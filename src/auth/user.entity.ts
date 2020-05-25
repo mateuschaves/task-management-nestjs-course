dab52d216df77769b3da1fe01cb177d91fb81636
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-05-25 05:10:03 -0300

    validating duplicated username

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,7 +1,8 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
 
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
 
     @PrimaryGeneratedColumn()
