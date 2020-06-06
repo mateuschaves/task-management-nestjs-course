7e549fd4777f6f922bc45b6ada306e71e0a7d840
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-06 16:51:40 -0300

    user repository tests

---
+++ src/auth/user.repository.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from "@nestjs/testing";
+import { UserRepository } from './user.repository';
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+
+const mockCredentialsDto = { username: 'TestUsername', password: 'TestPassword' };
+
+describe('UserRepository', () => {
+
+    let userRepository;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                UserRepository
+            ]
+        }).compile();
+
+        userRepository = await module.get<UserRepository>(UserRepository);
+    });
+
+
+    describe('signUp', () => {
+
+        let save;
+        beforeEach(() => {
+            save = jest.fn();
+            userRepository.create = jest.fn().mockReturnValue({ save });
+        });
+
+        it('successfully signs up the user', () => {
+            save.mockResolvedValue({});
+            expect(userRepository.signUp(mockCredentialsDto)).resolves.not.toThrow();
+        })
+
+        it('throws a conflict exception as username already exists', () => {
+            save.mockRejectedValue({ code: '23505' });
+            expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(ConflictException);
+        });
+
+        it('throws a internal server error exception on ungandled errors', () => {
+            save.mockRejectedValue({ code: '32505' });
+            expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(InternalServerErrorException);
+        });
+    });
+});
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -11,7 +11,7 @@ export class UserRepository extends Repository<User> {
 
         const salt = await bcrypt.genSalt();
 
-        const user = new User();
+        const user = this.create();
         user.username = username;
         user.salt = salt;
         user.password = await this.hashPassword(password, user.salt);
