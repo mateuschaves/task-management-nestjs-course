65e2d5fcec349b028dcad70eb8d7f641875dbb51
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-06 17:15:46 -0300

    Testing UserRepository

--- src/auth/user.repository.spec.ts
+++ src/auth/user.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Test } from "@nestjs/testing";
 import { UserRepository } from './user.repository';
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
 
 const mockCredentialsDto = { username: 'TestUsername', password: 'TestPassword' };
 
@@ -42,4 +44,40 @@ describe('UserRepository', () => {
             expect(userRepository.signUp(mockCredentialsDto)).rejects.toThrow(InternalServerErrorException);
         });
     });
+
+    describe('validateUserPassword', () => {
+
+        let user;
+
+        beforeEach(() => {
+            userRepository.findOne = jest.fn();
+
+            user = new User();
+            user.username = 'TestUsername';
+            user.validatePassword = jest.fn();
+        });
+
+        it('returns the username as validation is successfull', async () => {
+            userRepository.findOne.mockResolvedValue(user);
+            user.validatePassword.mockResolvedValue(true);
+
+            const result = await userRepository.validateUserPassword(mockCredentialsDto);
+            expect(result).toEqual('TestUsername');
+        });
+
+        it('returs null as user cannot be found', async () => {
+            userRepository.findOne.mockResolvedValue(null);
+            const result = await userRepository.validateUserPassword(mockCredentialsDto);
+            expect(user.validatePassword).not.toHaveBeenCalled();
+            expect(result).toBeNull();
+        });
+
+        it('returns null as password is invalid', async () => {
+            userRepository.findOne.mockResolvedValue(user);
+            user.validatePassword.mockResolvedValue(false);
+            const result = await userRepository.validateUserPassword(mockCredentialsDto);
+
+            expect(result).toBeNull();
+        });
+    })
 });
